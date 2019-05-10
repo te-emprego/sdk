e8f0c057fdb5ecdb6dcebd30f620c3e812994ca7
Daniel Bonifacio <contato@danielbonifacio.com.br>
2019-05-10 02:10:22 -0300

    token default encode/decode secret

--- modules/token.js
+++ modules/token.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 const token = {
+  secret: process.env.SECRET || 'development',
   /**
    * Encode some object
    * @param {Any} toEncode
    * @param {String} secret
    */
-  async encode(toEncode, secret) {
-    secret = secret || process.env.SECRET || 'development';
+  async encode(toEncode) {
     const options = {
       expiresIn: '1h',
     };
 
     const encoded = jwt
-      .sign(toEncode, secret, options);
+      .sign(toEncode, this.secret, options);
 
     return encoded;
   },
@@ -23,9 +23,9 @@ const token = {
    * @param {Any} toDecode
    * @param {String} secret
    */
-  async decode(toDecode, secret) {
+  async decode(toDecode) {
     try {
-      const decoded = jwt.decode(toDecode, secret);
+      const decoded = jwt.decode(toDecode, this.secret);
       return decoded;
     } catch (error) {
       throw Error(`Could not decode token. ${error.message}`);
@@ -37,9 +37,9 @@ const token = {
    * @param {Any} toDecode
    * @param {String} secret
    */
-  async verify(toVerify, secret) {
+  async verify(toVerify) {
     try {
-      const verified = jwt.verify(toVerify, secret);
+      const verified = jwt.verify(toVerify, this.secret);
       return verified;
     } catch (error) {
       throw Error(`Couldn't authenticate token. ${error.message}`);
